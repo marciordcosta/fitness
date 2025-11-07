a7641d3efe2d09fd0e2805d1703de8b3d5281cfd
Marcio Rodrigues <marciordcosta@gmail.com>
2025-11-07 14:48:15 -0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,3 +0,0 @@
-const supabaseUrl = "https://kkrbmuwwaxejfdqiczee.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtrcmJtdXd3YXhlamZkcWljemVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0ODc4MjYsImV4cCI6MjA3ODA2MzgyNn0.YtBWvo2DA9x49dN5C14jHRxWD4Hes7tOMnxMAdED86A";
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
