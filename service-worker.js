68547e4471d7d8b78124c04333cbc610a281ab36
Marcio Rodrigues <marciordcosta@gmail.com>
2025-11-13 09:28:00 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
-self.addEventListener("install", () => {
-  console.log("Service Worker instalado");
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
 });
