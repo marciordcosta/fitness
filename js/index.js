8f2572f553633cbd29365f98470133ea7c79d9b1
Marcio Rodrigues <marciordcosta@gmail.com>
2025-12-04 12:09:02 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -841,6 +841,14 @@ function gerarLinhasSemanais(lista) {
     return anotacoes;
 }
 
+Date.prototype.getWeek = function () {
+    const d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
+    const dayNum = d.getUTCDay() || 7;
+    d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+    const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+    return Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+};
+
 
 /* ======= CÁLCULO DE SEMANAS E MÉDIAS ======= */
 function calcularSemanasEMedias() {
